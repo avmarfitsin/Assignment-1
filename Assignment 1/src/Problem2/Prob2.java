af28ed260137d39cc52d79b0b055e60c1bb3b2d1
avmarfitsin <43153905+avmarfitsin@users.noreply.github.com>
2018-09-21 15:51:24 -0400

    Revert "lol"
    This reverts commit c4aea7a320b889427e424f9f3d241a27d236ea86.

--- Assignment 1/src/Problem2/Prob2.java
+++ Assignment 1/src/Problem2/Prob2.java
@@ -3,22 +3,15 @@ package Problem2;
 public class Prob2 extends abstractClassProb2 {
 public static void main(String[] args)
 {
-	Push("t1");
-	Push("t2");
-	Push("t3");
-	Push("t4");
-	Enqueue("t5");
-	Dequeue();
-	//Print();
-	//System.out.println(Traverse(3));
+	Push("xd");
+	Push("jej");
+	Push("no szchef");
+	Push("thing4");
+	Print();
 	System.out.println(Pop());
 	System.out.println(Pop());
 	System.out.println(Pop());
 	System.out.println(Pop());
-	System.out.println(Pop());
-	System.out.println(Dequeue());
-	System.out.println(Dequeue());
-	System.out.println(Dequeue());
 
 }
 }
--- Assignment 1/src/Problem2/abstractClassProb2.java
+++ Assignment 1/src/Problem2/abstractClassProb2.java
@@ -20,78 +20,28 @@ static void Push(Object o)
 	}
 	out=arr;
 }
-
-
 static Object Pop()
 {
-try
-{
+	
 	Object temp=out[0];
-size--;
-Object[] arr=new Object[size];
-for(int i=0; i<size;i++)
-{
-	arr[i]=out[i+1];
-}
-out=arr;
-return temp;
-}
-catch (Exception e)
-{
-	System.out.println("Empty array");
-}
-return null;
-}
-
-
-static void Enqueue(Object o)
-{
-	size++;
-	Object[] arr=new Object[size];
+	Object[] arr= {size-1};
+	size--;
 	for(int i=0;i<size-1;i++)
 	{
-		arr[i]=out[i];
+		arr[i]=out[i+1];
 	}
-	arr[size-1]=o;
-	out=arr;
-}
 
-
-static Object Dequeue()
-{
-try
-{
-	Object temp=out[size-1];
-	size--;
-	Object[] arr=new Object[size];
-	for(int i=0;i<size;i++)
-	{
-		arr[i]=out[i];
-	}
 	out=arr;
 	return temp;
 }
-catch (Exception e)
+static void Enqueue()
 {
-	System.out.println("Empty array");
+	
 }
-return null;
-}
-
-static Object Traverse(int i)
+static void Dequeue()
 {
-try
-{
-	return out[i];
+	
 }
-catch (Exception e)
-{
-System.out.println("Invalid input");
-}
-return null;
-}
-
-
 static void Print()
 {
 	System.out.println("Out:");
