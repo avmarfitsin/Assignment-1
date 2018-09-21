6d06f3d6b20748b77a8b00d1eeca9b230ed9bb95
avmarfitsin <43153905+avmarfitsin@users.noreply.github.com>
2018-09-21 15:55:17 -0400

    v3

--- Assignment 1/src/Problem3/abstractClassProb3.java
+++ Assignment 1/src/Problem3/abstractClassProb3.java
@@ -1,5 +1,32 @@
 package Problem3;
 
-public class abstractClassProb3 {
+public class abstractClassProb3 
+{
+	static class Node
+	{
+		 char ch;
+		 Node next;
+	}
+	
+	private static Node top;
+	static int size;
+	private void ArbitraryQueue()
+	{
+		size=0;
+		top=null;
+	}
+	private static void Push(char c)
+	{
+		Node topold=top;
+		top=new Node();
+		top.ch=c;
+		top.next=topold;
+	}
+	private static char Pop()
+	{
+		Node topold=top;
+		top=top.next;
+		return topold.ch;
+	}
 
 }
